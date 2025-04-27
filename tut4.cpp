154c595ac6a0843342a1e0598570f69a2f8c4da8
ubedkhn <ubedkhanuit@gmail.com>
2025-04-28 00:51:45 +0530

    Cpp tutorial

---
+++ tut4.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int glo = 6; /*this is global variable*/
+void sum(){ // void: written nothing
+    int a;
+}
+
+int main ()
+{ 
+    int glo=9; /*this is */
+    int a = 4;
+    int b = 5;
+    float pi = 3.15;
+    char c = 'd'; //only one variable used in char
+    bool y = true;
+sum();
+cout<<glo;
+
+        cout<< " this is tutorial 4 of c++ programming language.\n here the value of a is "<<a<<".\n The vlaue of b is "<<b; 
+        cout<< "\n the value of pi is : "<<pi;
+        cout<< "\n value of c is: "<<c; 
+ return 0; 
+
+}
+/*variable range in cpp from 1 to 255 char
+variable name begin with letter aplhabet or underscore
+after initial letter varibale name can be contain letters and no.
+variable name are case sensetive 
+no space and special char allowed
+cant use cpp keyword as a variable name */
---
+++ tut5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int num1, num2;
+cout<<"Enter the value of num1:\n";
+ /*"<<" is called the insertion operator and is used to print  */
+cin>>num1; /*'>>' is called the extraction operator and is used to take input from the user */
+
+cout<<"Enter the value of num2:\n";
+ /*"<<" id vsllrf indrtyion operator ie used to print  */
+cin>>num2; /*'>>' is called the extraction operator and is used to take input from the user */
+
+
+cout<<"the sum is "<<num1+num2;
+return 0;
+}
