bdc3c6732059c40b25c72f848b62485b75e2f792
Ubed Khan <ubedkhanuit@gmail.com>
2025-05-02 16:56:35 +0530

    Add files via upload

---
+++ tut0.cpp
@@ -0,0 +1,60 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int a,b,c;
+    // cout<<"enter the value of a:\n";
+    // cin>>a;
+    // cout<<"enter the value of b:\n";
+    // cin>>b;
+    // cout<<"enter the value of c:\n";
+    // cin>>c;
+
+    // cout<<"sum of a+b+c is :"<<a+b+c;
+
+    // return 0;
+
+//     int x=12,y=10,z=5;
+//     cout<<"the value of x is:"<<x<<endl;
+//     cout<<"the value of y is:"<<y<<endl;
+//     cout<<"the value of z is:"<<z<<endl;
+    
+
+// cout<<"the value of x==y is :" <<(x==y)<<endl;
+// cout<<"the value of x!=y is :"<<(x!=y)<<endl; 
+// cout<<"the value of z<x is:"<<(z<x)<<endl;
+
+// cout<< "the value of ((x==y) && (z<x)) is:"<<((x==y)&&(z<x))<<endl;
+
+// int age;
+
+// cout<<"enter your age:";
+// cin>>age;
+// if(age<18){
+//     cout<<"you are not allowed to the party"<<endl;
+// }
+
+// else if(age==18){
+//     cout<<"you are a kid and get kid pass for the party"<<endl;
+
+// }
+// else{
+//     cout<<"you are welcome to the party!!"<<endl;
+// }
+
+int age;
+cout<<"enter your age :"<<endl;
+cin>>age;
+if(age<18){
+    cout<<"you are not eligible for vote"<<endl;
+}
+else if(age==18){
+    cout<<"you are eligible for vote Congratulations"<<endl;
+}
+else{
+    cout<<"you are senior citizen of our country and eligible for vote🧓🏻/👵🏻"<<endl;
+}
+return 0;
+
+}
---
+++ tut2.cpp
@@ -0,0 +1,11 @@
+#include<iostream> //header file 
+using namespace std; //using namespace std - to avoid using std:: before every standard library function
+// std::cout - standard output stream, used to print to the console 
+int main()// int - integer , main - entry point of the program
+
+{ /*variables - containers to  store data*/
+    /*int a = 5; integer variable a initialized to 5  char - to store character  
+      */
+     
+    return 0;
+}
